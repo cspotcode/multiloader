e150757675fbfa40d2f444780c69b8ae16969e63
Andrew Bradley <cspotcode@gmail.com>
2022-03-14 11:54:24 -0400

    WIP

--- loader.mjs
+++ loader.mjs
@@ -10,44 +10,61 @@ function debugLog(...args) {
 const [commaSeparatedLoaders] = [...selfUrl.searchParams.keys()];
 const loaderSpecifiers = commaSeparatedLoaders.split(',').filter(s => s);
 debugDir(selfUrl.toString());
-const loaders = await Promise.all(loaderSpecifiers.map(loaderSpecifier => import(loaderSpecifier)));
 
-// TODO feature-detect node version; switch between loader APIs
-export async function resolve(specifier, context, nodeDefaultResolve) {
-    return await invokeLoaderResolve(0, specifier, context);
-    async function invokeLoaderResolve(startingIndex, specifier, context) {
-        let index;
-        for(index = startingIndex; index < loaders.length; index++) {
-            if(typeof loaders[index].resolve === 'function') break;
-        }
-        if(index >= loaders.length) {
-            return await nodeDefaultResolve(specifier, context, nodeDefaultResolve);
-        } else {
-            debugLog(`invoking resolve function for loader index ${index} named ${loaderSpecifiers[index]}`);
-            const resolveFunction = loaders[index].resolve;
-            return await resolveFunction(specifier, context, async (specifier, context, defaultResolve) => {
-                // loader tried to delegate to the default
-                return await invokeLoaderResolve(index + 1, specifier, context);
-            });
-        }
+const loaderPromises = loaderSpecifiers.map(loaderSpecifier => import(loaderSpecifier));
+const loaderPromises = [];
+const loaders = [];
+
+let loadersInitialized = null;
+const loadersApi = {
+    add(hooks, options) {
+        const {force = false} = options ?? {};
+        if(force !== true) throw new Error('Adding loaders without forcing is not implemented yet');
+        loadersInitialized = Promise.resolve(loadersInitialized).then(() => {
+            loaderPromises.push(hooks);
+            loaders = await Promise.all(loaderPromises);
+            invokers = createInvokers();
+        });
+        return loadersInitialized;
     }
 }
 
+process.loaders = loadersApi;
+
+// TODO feature-detect node version; switch between loader APIs
+
+export async function resolve(specifier, context, nodeDefaultResolve) {
+    return await invokers.resolve(0, specifier, context, nodeDefaultResolve);
+}
+
 export async function load(url, context, nodeDefaultLoad) {
-    return await invokeLoaderLoad(0, url, context);
-    async function invokeLoaderLoad(startingIndex, specifier, context) {
+    return await invokers.load(0, url, context, nodeDefaultLoad);
+}
+
+let invokers = createInvokers();
+function createInvokers() {
+    return {
+        resolve: createInvoker('resolve', loaders),
+        load: createInvoker('load', loaders)
+    };
+}
+
+function createInvoker(hookName, loaders) {
+    return invoker;
+    async function invoker(startingIndex, specifier, context, defaultHook) {
         let index;
         for(index = startingIndex; index < loaders.length; index++) {
-            if(typeof loaders[index].load === 'function') break;
+            if(typeof loaders[index][hookName] === 'function') break;
         }
         if(index >= loaders.length) {
-            return await nodeDefaultLoad(url, context, nodeDefaultLoad);
+            debugLog(`invoking node's default ${hookName} function`);
+            return await defaultHook(specifier, context, defaultHook);
         } else {
-            const loadFunction = loaders[index].load;
-            debugLog(`invoking load function for loader index ${index} named ${loaderSpecifiers[index]}`);
-            return await loadFunction(url, context, async (url, context, defaultLoad) => {
+            debugLog(`invoking ${hookName} function for loader index ${index} named ${loaderSpecifiers[index]}`);
+            const resolveFunction = loaders[index][hookName];
+            return await resolveFunction(specifier, context, async (specifier, context, _defaultHook) => {
                 // loader tried to delegate to the default
-                return await invokeLoaderLoad(index + 1, url, context);
+                return await invokeLoaderResolve(index + 1, specifier, context, defaultHook);
             });
         }
     }
